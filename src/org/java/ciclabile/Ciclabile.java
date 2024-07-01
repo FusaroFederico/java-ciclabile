6d25c32a7c9cf4aca21775ba08e76e1fe84ee687
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-07-01 15:59:19 +0200

    Add new class Ciclabile.java

---
+++ src/org/java/ciclabile/Ciclabile.java
@@ -0,0 +1,14 @@
+package org.java.ciclabile;
+
+public class Ciclabile {
+	// attributi
+	private int[] interi;
+	private int elementoCorrente;
+	
+	// costruttore
+	public Ciclabile(int[] interi) {
+		this.interi = interi;
+		this.elementoCorrente = 0;
+	}
+
+}
